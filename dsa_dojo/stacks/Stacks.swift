48819b96c12e79d9da5f998f2ce6843167416140
Taylor Pubins <tpubz@icloud.com>
2023-11-07 20:27:29 -0700

    Add #validateHTML func for validating tags

--- dsa_dojo/stacks/Stacks.swift
+++ dsa_dojo/stacks/Stacks.swift
@@ -1,3 +1,4 @@
+
 //
 //  Stacks.swift
 //  dsa_dojo
@@ -13,9 +14,10 @@ struct Stacks {
         var stack: [Character] = []
         
         for char in d {
+            // chars not '(' or '[' or '{' get ignored
             if char == "(" || char == "[" || char == "{" {
                 stack.append(char)
-            } else {
+            } else if char == ")" || char == "]" || char == "}" {
                 if char == ")" && stack.last == "(" {
                     stack.removeLast()
                 } else if char == "]" && stack.last == "[" {
@@ -24,6 +26,51 @@ struct Stacks {
                     stack.removeLast()
                 } else {
                     return false
+                }
+            }
+        }
+        
+        if stack.isEmpty {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    static func validateHTML(d: String) -> Bool {
+        var stack: [String] = []
+        var tagIsOpen: Bool = false
+        var tag: String = ""
+        
+        for char in d {
+            if !tagIsOpen {
+                if char == "<" {
+//                    tag.append(char)
+                    tagIsOpen = true
+                }
+            } else {  // tag is open
+                if char != ">" {  // not end of tag yet
+                    if char == " " {
+                        stack.append(tag)
+                        tag = ""
+                        tagIsOpen = false
+                        continue
+                    }
+                    tag.append(char)
+                } else {  // char == '>' a.k.a. end of tag
+                    // evaluate for match if tag contains '/'
+                    if tag.contains("/") {  // closing tag
+                        guard let openTag = stack.popLast() else {
+                            return false
+                        }
+                        if openTag != tag.dropFirst() {
+                            return false
+                        }
+                    } else {  // not a closing tag
+                        stack.append(tag)
+                    }
+                    tag = ""
+                    tagIsOpen = false
                 }
             }
         }
--- dsa_dojo/stacks/tests/StacksTests.swift
+++ dsa_dojo/stacks/tests/StacksTests.swift
@@ -23,11 +23,61 @@ final class StacksTest: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-        let stack_t: Bool = Stacks.validate(d: "(([]){()}())()")
+        let stack_t: Bool = Stacks.validate(d: "(([]){a()}())()")
         XCTAssertTrue(stack_t)
         var stack_f = Stacks.validate(d: "(()()())(]")
         XCTAssertFalse(stack_f)
         stack_f = Stacks.validate(d: "([}()()())()")
+        XCTAssertFalse(stack_f)
+    }
+    
+    func testValidatesHTML() throws {
+        // simple
+        var html = "<p>test</p>"
+        var stack_t: Bool = Stacks.validateHTML(d: html)
+        XCTAssertTrue(stack_t)
+        // nested
+        html = """
+            <section>
+                <p>test</p>
+            </section>
+            """
+        stack_t = Stacks.validateHTML(d: html)
+        XCTAssertTrue(stack_t)
+        // tag elements
+        html = """
+            <section id="test">
+                <p>test</p>
+            </section>
+            """
+        stack_t = Stacks.validateHTML(d: html)
+        XCTAssertTrue(stack_t)
+        // unclosed tag
+        html = """
+            <section id="test">
+            <div>
+                <p>test</p>
+            </section>
+            """
+        var stack_f = Stacks.validateHTML(d: html)
+        XCTAssertFalse(stack_f)
+        // misplaced closing tag
+        html = """
+            <section id="test">
+                <p>test</p>
+            </section>
+                        </div>
+            """
+        stack_f = Stacks.validateHTML(d: html)
+        XCTAssertFalse(stack_f)
+        // not enough closing tags
+        html = """
+            <section id="test">
+                <p>test</p>
+            </section>
+            <div>
+            """
+        stack_f = Stacks.validateHTML(d: html)
         XCTAssertFalse(stack_f)
     }
 
